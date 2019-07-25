0e4e6374da7a33e331c983a78bae83c4d6190bfd
MarcoMulato <marco.antonio.mulato@gmail.com>
2019-07-25 09:18:35 -0500

    Travis

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hola mundo!'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Server corriendo en el puerto:  ${port}!`))
